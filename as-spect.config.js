5ab1bd407f2174e5bacb6af5007fdaef6fae3c48
Joshua <tenner.joshua@gmail.com>
2019-02-12 17:41:51 -0500

    add config file init

--- as-spect.config.js
+++ as-spect.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  include: ["assembly/tests/**/*.spec.ts"],
-  imports: {
-
-  },
+  /**
+   * A set of globs passed to the glob package that qualify typescript files for testing.
+   */
+  include: ["assembly/__tests__/**/*.spec.ts"],
+  /**
+   * A set of regexp that will disclude source files from testing.
+   */
+  disclude: [],
+  /**
+   * Add your required AssemblyScript imports here.
+   */
+  imports: {},
 };
