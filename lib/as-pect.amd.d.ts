a25de75f112e16a917051368a7263a354930507c
jtenner <tenner.joshua@gmail.com>
2019-02-14 11:44:03 -0500

    compile test suite

--- lib/as-pect.amd.d.ts
+++ lib/as-pect.amd.d.ts
@@ -107,13 +107,38 @@ declare module "test/TestGroup" {
 declare module "test/TestSuite" {
     import { TestGroup } from "test/TestGroup";
     export class TestSuite {
+        /**
+         * All the test groups in this test suite.
+         */
         testGroups: TestGroup[];
+        /**
+         * The total number tests run in this test suite.
+         */
         totalTests: number;
+        /**
+         * The total number test successes in this test suite.
+         */
         successCount: number;
+        /**
+         * The total number of test fails in this test suite.
+         */
         failCount: number;
+        /**
+         * The total number to todos left to complete in this test suite.
+         */
         todoCount: number;
+        /**
+         * The test suite filename.
+         */
         filename: string;
+        /**
+         * The total time it took for this test suite to run in milliseconds rounded to the nearest
+         * thousandth.
+         */
         time: number;
+        /**
+         * An indicator if the test suite passed.
+         */
         pass: boolean;
     }
 }
--- lib/test/TestSuite.d.ts
+++ lib/test/TestSuite.d.ts
@@ -1,12 +1,37 @@
 import { TestGroup } from "./TestGroup";
 export declare class TestSuite {
+    /**
+     * All the test groups in this test suite.
+     */
     testGroups: TestGroup[];
+    /**
+     * The total number tests run in this test suite.
+     */
     totalTests: number;
+    /**
+     * The total number test successes in this test suite.
+     */
     successCount: number;
+    /**
+     * The total number of test fails in this test suite.
+     */
     failCount: number;
+    /**
+     * The total number to todos left to complete in this test suite.
+     */
     todoCount: number;
+    /**
+     * The test suite filename.
+     */
     filename: string;
+    /**
+     * The total time it took for this test suite to run in milliseconds rounded to the nearest
+     * thousandth.
+     */
     time: number;
+    /**
+     * An indicator if the test suite passed.
+     */
     pass: boolean;
 }
 //# sourceMappingURL=TestSuite.d.ts.map
