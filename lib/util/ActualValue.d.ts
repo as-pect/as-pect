375883e7b5e8dd0d9554477ae846e0b9311bf29e
jtenner <tenner.joshua@gmail.com>
2019-02-15 12:46:38 -0500

    update documentation, finish

--- lib/util/ActualValue.d.ts
+++ lib/util/ActualValue.d.ts
@@ -1,5 +1,12 @@
 import { LogValue } from "./LogValue";
+/**
+ * A class representing a reported expected or actual value. It shares a lot of properties with
+ * LogValue, so those are copied over.
+ */
 export declare class ActualValue extends LogValue {
+    /**
+     * An indicator if the actual expected value is negated.
+     */
     negated: boolean;
 }
 //# sourceMappingURL=ActualValue.d.ts.map
