373279523aabd904391b702b3d28f98ec4cb9523
Joshua Tenner <tenner.joshua@gmail.com>
2019-05-17 12:08:11 -0400

    Update lib

---
+++ lib/cli/util/collectPerformanceConfiguration.d.ts
@@ -0,0 +1,3 @@
+import { IYargs } from "./IYargs";
+export declare function collectPerformanceConfiguration(yargs: IYargs, performanceConfiguration: import("c:/Users/jtenner/Desktop/projects/as-pect/src/util/IPerformanceConfiguration").IPerformanceConfiguration): void;
+//# sourceMappingURL=collectPerformanceConfiguration.d.ts.map
--- lib/test.d.ts
+++ lib/test.d.ts
@@ -1,2 +1,1 @@
-export {};
 //# sourceMappingURL=test.d.ts.map
