2e98870e9542e3dd5e3196a91210539e56008dcd
Joshua Tenner <tenner.joshua@gmail.com>
2019-05-23 14:50:37 -0400

    udpate as-pect exports

--- src/as-pect.ts
+++ src/as-pect.ts
@@ -1,3 +1,4 @@
+export * from "./test/IWarning";
 export * from "./test/TestContext";
 export * from "./test/TestGroup";
 export * from "./test/TestReporter";
@@ -6,7 +7,9 @@ export * from "./reporter/DefaultTestReporter";
 export * from "./reporter/EmptyReporter";
 export * from "./reporter/SummaryTestReporter";
 export * from "./util/ActualValue";
+export * from "./util/IAspectExports";
 export * from "./util/IConfiguration";
 export * from "./util/ILogTarget";
+export * from "./util/IPerformanceConfiguration";
 export * from "./util/LogValue";
 export * from "./cli";
