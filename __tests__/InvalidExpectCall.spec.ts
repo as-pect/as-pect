678c1ee4a7af0d132f9df47520fcd8207b912db9
Joshua Tenner <tenner.joshua@gmail.com>
2019-05-24 13:53:30 -0400

    Update test fixtures

--- __tests__/InvalidExpectCall.spec.ts
+++ __tests__/InvalidExpectCall.spec.ts
@@ -22,38 +22,40 @@ describe("an invalid expect call test suite", () => {
       Array [
         Object {
           "message": "An expect() function call was used outside of a test function in assembly/jest.ts.",
-          "stackTrace": "    at assembly/internal/Expectation/Expectation<assembly/jest-invalid-expectation/SomeRef>#reportActual (wasm-function[44]:13)
-          at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[61]:22)
-          at assembly/internal/Describe/describe (wasm-function[63]:16)
-          at start:assembly/jest-invalid-expectation (wasm-function[64]:6)
-          at start (wasm-function[66]:1)",
+          "stackTrace": "    at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[58]:59)
+          at assembly/internal/Describe/describe (wasm-function[60]:16)
+          at start:assembly/jest-invalid-expectation (wasm-function[61]:6)
+          at start (wasm-function[64]:1)
+          at assembly/index/__main (wasm-function[62]:6)",
           "type": "InvalidExpectCall",
         },
         Object {
           "message": "An expect() function call was used outside of a test function in assembly/jest.ts.",
-          "stackTrace": "    at assembly/internal/Expectation/Expectation<assembly/jest-invalid-expectation/SomeRef>#reportActual (wasm-function[44]:79)
-          at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[61]:94)
-          at assembly/internal/Describe/describe (wasm-function[63]:16)
-          at start:assembly/jest-invalid-expectation (wasm-function[64]:6)
-          at start (wasm-function[66]:1)",
+          "stackTrace": "    at assembly/internal/comparison/truthyComparison/truthyComparison<assembly/jest-invalid-expectation/SomeRef> (wasm-function[48]:79)
+          at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[58]:197)
+          at assembly/internal/Describe/describe (wasm-function[60]:16)
+          at start:assembly/jest-invalid-expectation (wasm-function[61]:6)
+          at start (wasm-function[64]:1)
+          at assembly/index/__main (wasm-function[62]:6)",
           "type": "InvalidExpectCall",
         },
         Object {
           "message": "An expect() function call was used outside of a test function in assembly/jest.ts.",
-          "stackTrace": "    at assembly/internal/Expectation/Expectation<i32>#reportActual (wasm-function[50]:6)
-          at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[61]:250)
-          at assembly/internal/Describe/describe (wasm-function[63]:16)
-          at start:assembly/jest-invalid-expectation (wasm-function[64]:6)
-          at start (wasm-function[66]:1)",
+          "stackTrace": "    at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[58]:259)
+          at assembly/internal/Describe/describe (wasm-function[60]:16)
+          at start:assembly/jest-invalid-expectation (wasm-function[61]:6)
+          at start (wasm-function[64]:1)
+          at assembly/index/__main (wasm-function[62]:6)",
           "type": "InvalidExpectCall",
         },
         Object {
           "message": "An expect() function call was used outside of a test function in assembly/jest.ts.",
-          "stackTrace": "    at assembly/internal/Expectation/Expectation<~lib/string/String>#reportActual (wasm-function[57]:35)
-          at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[61]:318)
-          at assembly/internal/Describe/describe (wasm-function[63]:16)
-          at start:assembly/jest-invalid-expectation (wasm-function[64]:6)
-          at start (wasm-function[66]:1)",
+          "stackTrace": "    at assembly/internal/comparison/truthyComparison/truthyComparison<~lib/string/String> (wasm-function[55]:44)
+          at start:assembly/jest-invalid-expectation~anonymous|0 (wasm-function[58]:345)
+          at assembly/internal/Describe/describe (wasm-function[60]:16)
+          at start:assembly/jest-invalid-expectation (wasm-function[61]:6)
+          at start (wasm-function[64]:1)
+          at assembly/index/__main (wasm-function[62]:6)",
           "type": "InvalidExpectCall",
         },
       ]
