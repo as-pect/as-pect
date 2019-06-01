e9b087babb43b063916d6ca24b0258a78c327da4
Joshua <tenner.joshua@gmail.com>
2019-06-01 00:54:20 -0400

    Tests finally report, need to fix expectations.

---
+++ assembly/internal/value/index.ts
@@ -0,0 +1,28 @@
+export const enum ValueType {
+  None = 0,
+  Float = 1,
+  Reference = 2,
+  String = 3,
+  Array = 4,
+  Null = 5,
+  Falsy = 6,
+  Truthy = 7,
+  Finite = 8,
+}
+
+export class Actual {
+  public static type: ValueType;
+  public static reference: ArrayBuffer;
+  public static value: f64;
+  public static offset: i32;
+  public static stack: i32;
+}
+
+export class Expected {
+  public static type: ValueType;
+  public static reference: ArrayBuffer;
+  public static value: f64;
+  public static offset: i32;
+  public static stack: i32;
+  public static negated: i32;
+}
