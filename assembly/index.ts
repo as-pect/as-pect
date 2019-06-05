529925526a9e91b455629414b9769cec2842c832
Joshua Tenner <tenner.joshua@gmail.com>
2019-06-05 15:35:23 -0400

    Fix exports and imports of `index.ts`

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,7 +2,20 @@
 
 import { describe } from "./internal/Describe";
 import { expect, Expectation } from "./internal/Expectation";
-import { test, it, afterAll, afterEach, beforeAll, beforeEach, todo } from "./internal/Test";
+import {
+  test,
+  it,
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  todo,
+  throws,
+  itThrows,
+  debug,
+  xit,
+  xtest,
+} from "./internal/Test";
 import { log } from "./internal/log";
 import {
   performanceEnabled,
@@ -20,6 +33,7 @@ import { Expected } from "./internal/report/reportExpected";
 export { __call } from "./internal/call";
 export { __sendActual } from "./internal/report/reportActual";
 export { __sendExpected } from "./internal/report/reportExpected";
+export { __ignoreLogs } from "./internal/log";
 
 // @ts-ignore: Decorators *are* valid here
 @start
