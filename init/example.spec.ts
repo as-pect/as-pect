48183ec1a5ab8c2dfb780ed3af3d08484e0d650e
Joshua Tenner <tenner.joshua@gmail.com>
2019-06-28 09:06:12 -0400

    [Examples] Add more init examples

--- init/example.spec.ts
+++ init/example.spec.ts
@@ -1,5 +1,35 @@
+class Vec3 {
+  constructor(
+    public x: f64 = 0,
+    public y: f64 = 0,
+    public z: f64 = 0,
+  ) {}
+}
 describe("example", () => {
-  it("should be truthy", () => {
-    expect<i32>(19 + 23).toBe(42, "42 is the meaning of life.");
+  it("should be 42", () => {
+    expect<i32>(19 + 23).toBe(42, "19 + 23 is 42");
+  });
+
+  it("should be the same reference", () => {
+    let ref = new Vec3();
+    expect<Vec3>(ref).toBe(ref, "Reference Equality");
+  });
+
+  it("should perform a memory comparison", () => {
+    let a = new Vec3(1, 2, 3);
+    let b = new Vec3(1, 2, 3);
+
+    expect<Vec3>(a).toStrictEqual(b, "a and b have the same values, (discluding child references)");
+  });
+
+  it("should compare strings", () => {
+    expect<string>("a=" + "200").toBe("a=200", "both strings are equal");
+  });
+
+  it("should compare values", () => {
+    expect<i32>(10).toBeLessThan(200);
+    expect<i32>(1000).toBeGreaterThan(200);
+    expect<i32>(1000).toBeGreaterThanOrEqual(1000);
+    expect<i32>(1000).toBeLessThanOrEqual(1000);
   });
 });
