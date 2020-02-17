2daf253a13f6575587393ad6da854f1fe11ba14d
Joshua Tenner <tenner.joshua@gmail.com>
2020-02-16 23:43:11 -0500

    re-add Expected class

---
+++ packages/assembly/assembly/internal/Expected.ts
@@ -0,0 +1,80 @@
+// @ts-ignore: Decorators *are* valid here!
+@external("__aspect", "reportInvalidExpectCall")
+declare function reportInvalidExpectCall(): void;
+
+// @ts-ignore: Decorators *are* valid here!
+@external("__aspect", "reportExpectedHostValue")
+declare function reportExpectedHostValue(id: i32, negated: i32): void;
+
+// @ts-ignore: Decorators *are* valid here!
+@external("__aspect", "reportExpectedTruthy")
+declare function reportExpectedTruthy(negated: i32): void;
+
+// @ts-ignore: Decorators *are* valid here!
+@external("__aspect", "reportExpectedFalsy")
+declare function reportExpectedFalsy(negated: i32): void;
+
+// @ts-ignore: Decorators *are* valid here!
+@external("__aspect", "reportExpectedFinite")
+declare function reportExpectedFinite(negated: i32): void;
+
+// @ts-ignore: Decorators *are* valid here!
+@external("__aspect", "clearExpected")
+declare function clearExpected(): void;
+
+@global
+export class Expected {
+  /**
+   * This value indicates if the test suite is currently running.
+   */
+  static ready: bool = false;
+
+  /**
+   * This method reports an expected value, and wether the expectation was negated.
+   *
+   * @param {T} expected - The expected value and it's type.
+   * @param {i32} negated - A value indicating if the expectation is negated (1 is true) (default: 0)
+   */
+  static report<T>(expected: T, negated: i32 = 0): void {
+    if (!Expected.ready) {
+      reportInvalidExpectCall();
+      return;
+    }
+
+    reportExpectedHostValue(Reflect.toHostValue(expected), negated);
+  }
+
+  /**
+   * Report an expected truthy value to the host, and if the expectation is negated.
+   *
+   * @param {i32} negated - A value, 1 or 0 indicating if the expectation is negated.
+   */
+  static reportTruthy(negated: i32 = 0): void {
+    reportExpectedTruthy(negated);
+  }
+
+  /**
+   * Report an expected falsy value to the host, and if the expectation is negated.
+   *
+   * @param {i32} negated - A value, 1 or 0 indicating if the expectation is negated.
+   */
+  static reportFalsy(negated: i32 = 0): void {
+    reportExpectedFalsy(negated);
+  }
+
+  /**
+   * Report an expected finite value to the host, and if the expectation is negated.
+   *
+   * @param {i32} negated - A value, 1 or 0 indicating if the expectation is negated.
+   */
+  static reportFinite(negated: i32 = 0): void {
+    reportExpectedFinite(negated);
+  }
+
+  /**
+   * Clear the expected value.
+   */
+  static clear(): void {
+    clearExpected();
+  }
+}
