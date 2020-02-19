c2462f4adce32a1c6982017ee2abbe66b5517c50
Joshua Tenner <tenner.joshua@gmail.com>
2020-02-19 10:03:59 -0500

    update package versions

--- packages/cli/lib/help.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @ignore
- *
- * This method prints the help text.
- */
-export declare function help(): void;
--- packages/core/lib/transform/createStrictEqualsMember.d.ts
+++
@@ -1,8 +0,0 @@
-import { ClassDeclaration, FunctionDeclaration } from "assemblyscript";
-/**
- * This method creates a single FunctionDeclaration that allows Reflect.equals
- * to validate normal class member values.
- *
- * @param {ClassDeclaration} classDeclaration - The class that requires a new function.
- */
-export declare function createStrictEqualsMember(classDeclaration: ClassDeclaration): FunctionDeclaration;
