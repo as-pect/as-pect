93b1ca6bc9af350190e83b646a845cf20bf82c64
Willem Wyndham <willem@cs.umd.edu>
2020-02-19 14:45:32 -0500

    Fix window paths
    Doesn't assume unix paths

--- packages/core/src/transform/assemblyscript.js
+++ packages/core/src/transform/assemblyscript.js
@@ -5,12 +5,11 @@ const path = require("path");
 let assemblyscriptPath = Object.getOwnPropertyNames(require.cache).filter(s => s.endsWith("assemblyscript.js"))[0];
 let transformerPath;
 if (assemblyscriptPath) {
-  let prefix = assemblyscriptPath.substring(0, assemblyscriptPath.lastIndexOf("/dist/assemblyscript.js"));
-  transformerPath =  path.join(prefix, "cli", "transform");
+  let splitPath = assemblyscriptPath.split(path.sep).slice(0, -2);
+  transformerPath =  splitPath.concat([ "cli", "transform"]).join(path.sep);
 } else {
   assemblyscriptPath = require.resolve("assemblyscript");
   transformerPath = require.resolve("assemblyscript/cli/transform");
-
 }
 //@ts-ignore
 module.exports.Transform = require(transformerPath).Transform;
