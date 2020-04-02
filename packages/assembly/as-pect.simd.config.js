fc1913155d26405b3748cb2924315e6fd2af2d48
Joshua Tenner <tenner.joshua@gmail.com>
2020-04-01 20:44:39 -0400

    update to nightly, add simd configuration

---
+++ packages/assembly/as-pect.simd.config.js
@@ -0,0 +1,40 @@
+const v8 = require("v8");
+v8.setFlagsFromString("–-experimental-wasm-simd");
+
+module.exports = {
+  /**
+   * A set of globs passed to the glob package that qualify typescript files for testing.
+   */
+  include: ["assembly/__tests__/**/*.spec.ts"],
+  /**
+   * A set of globs passed to the glob package that quality files to be added to each test.
+   */
+  add: ["assembly/__tests__/**/*.include.ts"],
+  /**
+   * All the compiler flags needed for this test suite. Make sure that a binary file is output.
+   */
+  flags: {
+    /** To output a wat file, uncomment the following line. */
+    "--textFile": ["output.wat"],
+    /** To select an appropriate runtime, use the --runtime compiler flag. */
+    "--runtime": ["full"], // Acceptable values are: full, half, stub (arena), and none
+    "--enable": ["simd"]
+  },
+  /**
+   * A set of regexp that will disclude source files from testing.
+   */
+  disclude: [/node_modules/i],
+  /**
+   * Add your required AssemblyScript imports here.
+   */
+  imports(memory, createImports, instantiateSync, binary) {
+    let instance; // Imports can reference this
+    const myImports = {
+      // put your web assembly imports here, and return the module
+    };
+    instance = instantiateSync(binary, createImports(myImports));
+    return instance;
+  },
+  /** Use the summary reporter. */
+  // reporter: new SummaryTestReporter(),
+};
