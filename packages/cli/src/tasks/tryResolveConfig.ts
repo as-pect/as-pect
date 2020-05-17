77d2afe6eac73d033adc706298b2bcb0185f39e4
Joshua Tenner <tenner.joshua@gmail.com>
2020-05-17 17:38:31 -0400

    more

--- packages/cli/src/tasks/tryResolveConfig.ts
+++ packages/cli/src/tasks/tryResolveConfig.ts
@@ -8,13 +8,20 @@ export async function tryResolveConfig(context: AspectRunContext): Promise<Aspec
     const configLocation = path.resolve(context.parsed.options.config);
     context.config = require(configLocation);
   } else {
-    const argv = context.argv;
+    const path = require("path");
+    let index = context.argv.indexOf("--");
+    const argv = index === -1
+      ? context.argv
+      : context.argv.slice(0, index);
 
     // we need to obtain the url for the as-pect config manually
-    let index = argv.indexOf("--config");
+    index = argv.indexOf("--config");
     if (index === -1) index = argv.indexOf("-c");
     // no as-pect configuration is required, and all required options are cli bound
-    if (index === -1) return context;
+    if (index === -1) {
+      context.config = require(path.resolve("./as-pect.config.js"));
+      return context;
+    }
 
     // a flag was passed, now we need to resolve the compiler, or throw an error.
     const nextIndex = index + 1;
@@ -29,6 +36,7 @@ export async function tryResolveConfig(context: AspectRunContext): Promise<Aspec
       process.exit(1);
       return context;
     }
+    context.config = require(path.resolve(argv[nextIndex]));
   }
 
   return context;
