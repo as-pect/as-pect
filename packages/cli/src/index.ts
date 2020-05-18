78cad0a3052412aed5126c9e1f4ee4192541cd2d
Joshua Tenner <tenner.joshua@gmail.com>
2020-05-18 13:49:28 -0400

    replace logo with simple version indicator

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,11 +5,10 @@ import { AspectRunContext } from "./AspectRunContext";
 import { assertCompiler } from "./tasks/assertCompiler";
 import { parseArgv } from "./tasks/parseArgv";
 import { assertNoUnknown } from "./tasks/assertNoUnkown";
-import { printError } from "./util/printError";
-import { AspectErrorCode } from "./util/AspectErrorCode";
 import { exit } from "./tasks/exit";
 import { unexpectedError } from "./tasks/unexpectedError";
 import { tryValidateConfig } from "./tasks/tryValidateConfig";
+import { help } from "assemblyscript/cli/util/options";
 
 export function asp(argv: string[], config: IProcessConfiguration): void {
   const context: AspectRunContext = {
@@ -18,12 +17,26 @@ export function asp(argv: string[], config: IProcessConfiguration): void {
     files: [],
     exitCode: 0,
   };
+
+  config.stdout.write("as-pect cli v" + require("../package.json").version + "\n\n");
+
   tryResolveCompiler(context)
     .then(tryResolveConfig)
     .then(tryValidateConfig)
     .then(assertCompiler)
     .then(parseArgv)
     .then(assertNoUnknown)
+    .then((context) => {
+      // parseArgv sets the parseResult property
+      const result = context.parseResult!;
+      const process = context.process;
+      const options = result.options;
+
+      if (options.help) {
+        const helpText = help(require("./options").options);
+        process.stdout.write(helpText + "\n");
+      }
+    })
     .catch((reason) => unexpectedError(reason, context))
     .finally(() => exit(context));
 }
