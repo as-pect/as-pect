6293fc363fd15f2810eec08eda427ea04802274f
Joshua Tenner <tenner.joshua@gmail.com>
2020-06-14 15:35:17 -0400

    wasi

--- packages/core/__tests__/WASI.spec.ts
+++ packages/core/__tests__/WASI.spec.ts
@@ -1,26 +1,48 @@
 import { TestContext } from "../src";
-import { createWasiModule } from "./setup/createWasiModule";
 import { VerboseReporterWrapper } from "./setup/VerboseReporterWrapper";
-const reporter = new VerboseReporterWrapper();
-let ctx: TestContext;
+import { promises as fs } from "fs";
+import { instantiate } from "assemblyscript/lib/loader";
+import { WASI } from "wasi";
 
-let start = new Promise<void>((resolve, reject) => {
-  createWasiModule({}, reporter, (err, result) => {
-    if (err) {
-      console.log(err);
-      reject(err);
-    } else {
-      ctx = result!;
-      resolve();
-    }
+test("snapshots", async () => {
+  const reporter = new VerboseReporterWrapper();
+  const binary = await fs.readFile("./assembly/jest-wasi.wasm");
+  const wasi = new WASI({
+    args: process.argv,
+    env: process.env,
+    preopens: {},
   });
-});
+  const ctx = new TestContext({
+    reporter,
+    binary,
+    fileName: "./assembly/jest-wasi.ts",
+    wasi,
+  });
+  wasi.start = jest.fn((instance: WebAssembly.Instance) => {
+    const symbols = Object.getOwnPropertySymbols(wasi);
+    const kStartedSymbol = symbols.filter((symbol) =>
+      symbol.toString().includes("kStarted"),
+    )[0];
+    const setMemorySymbol = symbols.filter((symbol) =>
+      symbol.toString().includes("setMemory"),
+    )[0];
 
-beforeEach(() => start);
-
-test("snapshots", () => {
+    // @ts-ignore: symbol access mock
+    wasi[setMemorySymbol](instance.exports.memory);
+    // @ts-ignore: symbol access mock
+    wasi[kStartedSymbol] = true;
+    // @ts-ignore: _start is an exported function
+    instance.exports._start();
+  });
+  ctx.run(await instantiate(binary, ctx.createImports({
+    __debug: {
+      log(value: number) {
+        console.log(value);
+      }
+    }
+  })));
   expect(ctx).toBeDefined();
-  // expect(ctx.pass).toBeTruthy();
+  expect(ctx.pass).toBeTruthy();
   for (const [name, values] of reporter.snapshots.entries()) {
     for (const value of values) {
       expect(value).toMatchSnapshot(name);
--- packages/core/assembly/jest-wasi.ts
+++ packages/core/assembly/jest-wasi.ts
@@ -1,8 +1,14 @@
 import { random_get } from "wasi";
 
+@external("__debug", "log")
+declare function debug(value: f64): void;
+
 let output = [0] as StaticArray<u64>;
 
 test("output", () => {
-  log(random_get(changetype<usize>(output), sizeof<u64>()));
-  expect(output[0]).not.toBe(0);
+  debug(1);
+  let a = random_get(changetype<usize>(output), sizeof<u64>());
+  debug(a);
+  log(a);
+  log(output[0]);
 });
