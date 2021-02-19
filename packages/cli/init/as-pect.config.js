ace6f4494b7c7329a697f73024d15b97c98e3730
bcolloran <brendan@colloran.net>
2021-02-19 08:18:35 -0800

    Update as-pect.config.js (#329)
    use  "incremental" for runtime value and change comment to mention AssemblyScript 0.18 runtime values

--- packages/cli/init/as-pect.config.js
+++ packages/cli/init/as-pect.config.js
@@ -14,7 +14,7 @@ module.exports = {
     /** To output a wat file, uncomment the following line. */
     // "--textFile": ["output.wat"],
     /** A runtime must be provided here. */
-    "--runtime": ["full"], // Acceptable values are: full, half, stub (arena), and none
+    "--runtime": ["incremental"], // Acceptable values are: "incremental", "minimal", and "stub"
   },
   /**
    * A set of regexp that will disclude source files from testing.
